50e3493ad12bb70ef900fc4b4c87eae5d8642061
Moses <moses.m917@gmail.com>
2025-07-15 10:07:33 +0200

    App.Jsx new code

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,51 @@ import './App.css'
 import ProductList from './ProductList';
 
 function App() {
-  const [products, setProducts]=useState([]);
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [searchItem, setSearchItem] = useState("");
+  const [sortOrder, setSortOrder] = useState("asc");
+
 
   useEffect(() => {
     fetch('http://localhost:8080/api/products')
       .then(response => response.json())
       .then(data => setProducts(data))
       .catch(error => console.error('Error fetching products:', error));
-  },[]);
+    // Fetch categories
+    fetch('http://localhost:8080/api/categories')
+      .then(response => response.json())
+      .then(data => setCategories(data))
+      .catch(error => console.error('Error fetching products:', error));
+  }, []);
+
+  const handleSearchChange = (event)=>{
+    setSearchItem(event.target.value);
+  }
 
 
   return (
-    <div className="container"> 
+    <div className="container">
+      <h1 className="my-4">Product List</h1>
+      <div className="row align-items mb-3">
+          <div className='col-md-4 col-sm-12 mb-12'>
+            <p>Category Filter</p>
+          </div>
+          <div>
+            <input type="text" className='form-control' placeholder='Search product'  onChange={handleSearchChange}/>
+            
+            
+          </div>
+      </div>
       <div>
         {products.length ? (
-          <ProductList products = {products}/>
-        ): (
+          <ProductList products={products} />
+        ) : (
           <p>No products available</p>
         )}
       </div>
-     
+
     </div>
   )
 }
